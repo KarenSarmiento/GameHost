eeb18fcadf7eb90311896b168f6d42da298a561a
Marco Selvatici <marcoselvatici98@gmail.com>
2019-01-20 00:50:49 +0000

    change server to use userId

--- Hub/PongHub.cs
+++ Hub/PongHub.cs
@@ -6,34 +6,37 @@ namespace GameHost.Hubs
     public class PongHub : Hub
     {
         private Dictionary<string, Pong> gameInstances = new Dictionary<string, Pong>();
+        private Dictionary<int, string> userIdToConnectionId = new Dictionary<int, string>(); 
         private int gameInstancePin = 0;
 
         // 1. Called by display to begin a game of Pong.
         // Then display waits for connection requests (with pin) from controllers.
-        public void startPong() {
+        public void startPong(int userId) {
+            updatedUserIdToConnectionIdMap(userId, Context.ConnectionId);
             string pin = generateRandomGamePin();
-            Pong pong = new Pong(Context.ConnectionId);
+            Pong pong = new Pong(getConnectionIdFromUserId(userId));
             gameInstances.Add(pin, pong);
-            Clients.Client(Context.ConnectionId).SendAsync("ackStartPong", pin);
+            Clients.Client(getConnectionIdFromUserId(userId)).SendAsync("ackStartPong", pin);
         }
 
         // 2. Controller attempts to conect to a game with a pin.
         // If pin false, then return FAILED
         // Else if the games is empty, wait for more player connection requests and return WAITING
         // If one player already is waiting return SUCCESS
-        public void controlConnectionRequest(string pin) {
+        public void controlConnectionRequest(int userId, string pin) {
+            updatedUserIdToConnectionIdMap(userId, Context.ConnectionId);
             Pong instance;
             bool existsGameInstance = gameInstances.TryGetValue(pin, out instance);
             if (existsGameInstance && !instance.isGameInProgress()) {
                 int numPlayersInGame = instance.getNumPlayersInGame();
                 if (numPlayersInGame == 0) {
-                    instance.setPOneID(Context.ConnectionId);
-                    Clients.Client(instance.getPOneID()).SendAsync("ackControlConnectionRequest", "WAIITNG");
+                    instance.setPOneID(userId);
+                    Clients.Client(getConnectionIdFromUserId(instance.getPOneID())).SendAsync("ackControlConnectionRequest", "WAIITNG");
                 } else {
-                    instance.setPTwoID(Context.ConnectionId);
-                    Clients.Client(instance.getPOneID()).SendAsync("ackControlConnectionRequest", "SUCCESS");
-                    Clients.Client(instance.getPTwoID()).SendAsync("ackControlConnectionRequest", "SUCCESS");
-                    Clients.Client(instance.getDisplayID()).SendAsync("ackControlConnectionRequest", "SUCCESS");
+                    instance.setPTwoID(userId);
+                    Clients.Client(getConnectionIdFromUserId(instance.getPOneID())).SendAsync("ackControlConnectionRequest", "SUCCESS");
+                    Clients.Client(getConnectionIdFromUserId(instance.getPTwoID())).SendAsync("ackControlConnectionRequest", "SUCCESS");
+                    Clients.Client(getConnectionIdFromUserId(instance.getDisplayID())).SendAsync("ackControlConnectionRequest", "SUCCESS");
                     instance.setGameInProgress(true);
                 }
                 instance.incrementNumPlayersInGame();
@@ -43,29 +46,30 @@ namespace GameHost.Hubs
         }
 
         // 3. In game, controller sends what arrows are pressed. This is redirected to display.
-        public void buttonPressed(string pin, string button) {
+        public void buttonPressed(int userId, string pin, string button) {
+            updatedUserIdToConnectionIdMap(userId, Context.ConnectionId);
             Pong instance;
             bool existsGameInstance = gameInstances.TryGetValue(pin, out instance);
             if (existsGameInstance) {
-                Clients.Client(instance.getDisplayID()).SendAsync("ackButtonPressed", button);
+                Clients.Client(getConnectionIdFromUserId(instance.getDisplayID())).SendAsync("ackButtonPressed", button);
             }
         }
 
         // 4. Display signals when game is over. This is redirected to controllers.
         public void endGame(string pin, string winner) {
+            updatedUserIdToConnectionIdMap(userId, Context.ConnectionId);
             Pong instance;
             bool existsGameInstance = gameInstances.TryGetValue(pin, out instance);
             if (existsGameInstance) {
                 if (string.Equals(winner, "1")) {
-                    Clients.Client(instance.getPOneID()).SendAsync("ackEndGame", "WINNER");
-                    Clients.Client(instance.getPTwoID()).SendAsync("ackEndGame", "LOSER");
+                    Clients.Client(getConnectionIdFromUserId(instance.getPOneID())).SendAsync("ackEndGame", "WINNER");
+                    Clients.Client(getConnectionIdFromUserId(instance.getPTwoID())).SendAsync("ackEndGame", "LOSER");
                 } else if (string.Equals(winner, "2")) {
-                    Clients.Client(instance.getPOneID()).SendAsync("ackEndGame", "LOSER");
-                    Clients.Client(instance.getPTwoID()).SendAsync("ackEndGame", "WINNER");
-
+                    Clients.Client(getConnectionIdFromUserId(instance.getPOneID())).SendAsync("ackEndGame", "LOSER");
+                    Clients.Client(getConnectionIdFromUserId(instance.getPTwoID())).SendAsync("ackEndGame", "WINNER");
                 } else {
-                    Clients.Client(instance.getPOneID()).SendAsync("ackEndGame", "UNKNOWN");
-                    Clients.Client(instance.getPTwoID()).SendAsync("ackEndGame", "UNKNOWN");
+                    Clients.Client(getConnectionIdFromUserId(instance.getPOneID())).SendAsync("ackEndGame", "UNKNOWN");
+                    Clients.Client(getConnectionIdFromUserId(instance.getPTwoID())).SendAsync("ackEndGame", "UNKNOWN");
                     System.Console.Error.WriteLine("Invalid endGame string received.");
                 }
                 gameInstances.Remove(pin);
@@ -75,6 +79,14 @@ namespace GameHost.Hubs
         private string generateRandomGamePin() {
             gameInstancePin++;
             return gameInstancePin.ToString();
+        }
+
+        private string getConnectionIdFromUserId(int userId) {
+            return userIdToConnectionId[userId];
+        }
+
+        private void updatedUserIdToConnectionIdMap(int userId, string connectionId) {
+            userIdToConnectionId[userId] = connectionId;
         }
     }
 }
